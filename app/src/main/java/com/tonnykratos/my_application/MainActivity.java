ebce62fba398f6aa8962d92029a3186c4a2afc4e
Tonny Kratos <tonnykratos14@gmail.com>
2022-11-02 09:55:10 +0300

    VPN to github

---
+++ app/src/main/java/com/tonnykratos/my_application/MainActivity.java
@@ -0,0 +1,27 @@
+package com.tonnykratos.my_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
